c3b7c7231c9785e8dc1d130d4a7b74cc123fa1a8
julianbaracat <julian.baracat@gmail.com>
2025-06-23 02:54:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, jsonify
 import main
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return jsonify(main.main({}))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
